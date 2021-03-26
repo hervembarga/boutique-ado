dc69319d070d9315ab58c5957f1497482faff78c
hervembarga <herve.mbarga@gmail.com>
2021-03-26 16:41:58 +0000

    Added production details functionality

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -69,7 +69,8 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', #required by Allauth
+                'django.template.context_processors.request', 
+                # required by Allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],

e843fc1ca8e5692a6f2a598bb9c3c8e4b9314d37
hervembarga <herve.mbarga@gmail.com>
2021-04-26 17:30:03 +0000

    Added login with facebook

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.github',
+    'allauth.socialaccount.providers.facebook',
     'home',
     'products',
     'bag',

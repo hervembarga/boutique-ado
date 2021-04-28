f6723257182dc05848b338fc73830a943ff4954a
hervembarga <herve.mbarga@gmail.com>
2021-04-28 19:16:29 +0000

    rename order to order2

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order, OrderLineItem
+from .models import Ordertwo, OrderLineItem
 
 
 class OrderLineItemAdminInline(admin.TabularInline):
@@ -12,13 +12,13 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total', 'grand_total',
-                       )
+                       'original_bag', 'stripe_pid')
 
     fields = ('order_number', 'user_profile', 'date', 'full_name', 'email',
               'phone_number', 'country', 'postcode', 'town_or_city',
               'street_address1', 'street_address2', 'county',
               'delivery_cost', 'order_total', 'grand_total',
-              'countytwo')
+              'original_bag', 'stripe_pid')
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost', 'grand_total',)
@@ -26,4 +26,4 @@ class OrderAdmin(admin.ModelAdmin):
     ordering = ('-date',)
 
 
-admin.site.register(Order, OrderAdmin)
+admin.site.register(Ordertwo, OrderAdmin)

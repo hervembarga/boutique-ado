83655b1c92b90c03e35523f267acadef167ede42
hervembarga <herve.mbarga@gmail.com>
2021-04-28 17:09:19 +0000

    test new table in checkout app

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order, OrderLineItem
+from .models import Order, OrderLineItem, Mbarga
 
 
 class OrderLineItemAdminInline(admin.TabularInline):
@@ -27,3 +27,4 @@ class OrderAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Order, OrderAdmin)
+admin.site.register(Mbarga)
